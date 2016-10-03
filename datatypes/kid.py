5661782f4ae2cec761728b1effd5af5fd4414da4
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-03 13:27:44 +0200

    Create kid.py

---
+++ datatypes/kid.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# kid.py
+
+import random
+
+male = False
+male = bool(random.randint(0, 1))
+
+if (male):
+   print ("We will use name John")
+else:
+   print ("We will use name Victoria")
