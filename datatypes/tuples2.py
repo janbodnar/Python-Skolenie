7f74cfd3c8146fb9a430aead79abf818e68aa59b
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-03 14:02:54 +0200

    Create tuples2.py

---
+++ datatypes/tuples2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# tuples2.py
+
+five = (1, 2, 3, 4, 5)
+
+print ("five[0] : ", five[0])
+print ("five[-1] : ", five[-1])
+print ("five[-2] : ", five[-2])
+print ("five[:] : ", five[:])
+print ("five[0:4] : ", five[0:4])
+print ("five[1:2] : ", five[1:2])
+print ("five[:2] : ", five[:2])
+print ("five[:-1] : ", five[:-1])
+print ("five[:9] : ", five[:9])
