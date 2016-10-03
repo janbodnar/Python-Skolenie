ece5122a7c803323213b73bbd4a9ada70da43ce2
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-03 18:45:05 +0200

    Create list_modify2.py

---
+++ datatypes/list_modify2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# list_modify2.py
+
+first = [1, 2, 3]
+second = [4, 5, 6]
+
+first.extend(second)
+print (first)
+
+first[0] = 11
+first[1] = 22
+first[2] = 33
+print (first)
+
+print (first.pop(5))
+print (first)
