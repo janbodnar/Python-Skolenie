3df18f21b22113b3a835a8c4458740c7a1c8852c
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-03 13:45:15 +0200

    Create sprinter.py

---
+++ datatypes/sprinter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# sprinter.py
+
+# 100m is 0.1 km
+
+distance = 0.1
+
+# 9.87s is 9.87/60*60 h
+
+time = 9.87 / 3600
+
+speed = distance / time
+
+print ("The average speed of a sprinter is", speed, "km/h")
