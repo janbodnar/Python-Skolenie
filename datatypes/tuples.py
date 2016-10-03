732d00791cb350f2991b763ee1b0e69bb91c253e
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-03 13:58:55 +0200

    Create tuples.py

---
+++ datatypes/tuples.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# tuples.py
+
+first = (1, 2, 3)
+second = (4, 5, 6)
+
+print ("len(first) : ", len(first))
+print ("max(first) : ", max(first))
+print ("min(first) : ", min(first))
+print ("first + second :", first + second)
+print ("first * 3 : ", first * 3)
+print ("1 in first : ", 1 in first)
+print ("5 not in second : ", 5 not in second)
