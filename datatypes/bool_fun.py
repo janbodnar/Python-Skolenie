6ae5cb94f8daeed0a75ac4c9b01b4bd76b72471a
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-03 13:33:17 +0200

    Create bool_fun.py

---
+++ datatypes/bool_fun.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# bool_fun.py
+
+print (bool(True))
+print (bool(False))
+print (bool("text"))
+print (bool(""))
+print (bool(' '))
+print (bool(0))
+print (bool())
+print (bool(3))
+print (bool(None))
