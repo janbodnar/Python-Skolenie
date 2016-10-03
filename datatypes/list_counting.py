c6271354ef8fd3bfd5a3f08f2e9e84f1adb3c519
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-03 16:03:51 +0200

    Create list_counting.py

---
+++ datatypes/list_counting.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# list_counting.py
+
+numbers = [0, 0, 2, 3, 3, 3, 3]
+
+print ("zero is here",  numbers.count(0), "times")
+print ("one is here",   numbers.count(1), "times") 
+print ("two is here",   numbers.count(2), "time") 
+print ("three is here", numbers.count(3), "times") 
