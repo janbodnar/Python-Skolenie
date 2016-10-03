0d3314af30e9a9a8fcbedd12414259d07799e0fc
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-03 13:42:20 +0200

    Create apples.py

---
+++ datatypes/apples.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# number of baskets
+baskets = 16
+
+# number of apples in a basket
+apples_in_basket = 24
+
+# we get the total number of apples
+total = baskets * apples_in_basket
+
+
+print ("There are total of", total, "apples")
