04ae206fbf3dc196ece862b01f49b57e2dfbc2bb
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-03 14:05:33 +0200

    Create tuples_mix.py

---
+++ datatypes/tuples_mix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# tuples_mix.py
+
+mix = (1, 2, "solaris", (1, 2, 3))
+
+print ("mix[1] :", mix[1])
+print ("mix[2] :", mix[2])
+print ("mix[3] :", mix[3])
+print ("mix[3][0] :", mix[3][0])
+print ("mix[3][1] :", mix[3][1])
+print ("mix[3][2] :", mix[3][2])
