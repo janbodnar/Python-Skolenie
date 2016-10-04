3d88e1b8bdcf2aa6d5e4b896f78a2bdfa1fef93e
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-04 14:55:52 +0200

    Create substrings.py

---
+++ strings/substrings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# substrings.py
+
+a = "I saw a wolf in the forest. A lone wolf."
+
+print (a.find("wolf"))
+print (a.find("wolf", 10, 20))
+print (a.find("wolf", 15))
+
+print (a.rfind("wolf"))
