86716f9e841ca000ce91f30f61aec59cd68e953f
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-04 13:39:00 +0200

    Create quotes.py

---
+++ strings/quotes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# quotes.py
+
+print ("There are many stars.")
+print ("He said, \"Which one is your favourite?\"")
+
+print ('There are many stars.')
+print ('He said, "Which one is your favourite?"')
