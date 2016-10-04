a8f6e715d3f1f6be761a78b6091025d7d6e0a914
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-04 14:35:01 +0200

    Create string_elements.py

---
+++ strings/string_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# string_elements.py
+
+s = "Eagle"
+
+print (s[0])
+print (s[4])
+print (s[-1])
+print (s[-2])
+
+print ("****************")
+
+print (s[0:4])
+print (s[1:3])
+print (s[:])
