6c48be987e68c5ee2d3be69fb584b2bc6fcac9e9
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-04 14:00:47 +0200

    Create stripping.py

---
+++ strings/stripping.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# strippig.py
+
+s = " Eagle  "
+
+s2 = s.rstrip()
+s3 = s.lstrip()
+s4 = s.strip()
+
+print ('{0} {1}'.format(s, len(s)))
+print ('{0} {1}'.format(s2, len(s2)))
+print ('{0} {1}'.format(s3, len(s3)))
+print ('{0} {1}'.format(s4, len(s4)))
