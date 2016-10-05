faec4bbd06381cd906ffdb6144ab99879b5150f5
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-05 12:28:04 +0200

    Update and rename teams.py to teams1.py

---
+++ strings/teams1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# teams1.py
+
+print ("Ajax Amsterdam" " - " + "Inter Milano " "2:3")
+print ("Real Madridi" " - " "AC Milano " "3:3")
+print ("Dortmund" " - " "Sparta Praha " "2:1")
