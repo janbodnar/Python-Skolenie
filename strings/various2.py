224d2c2aa18e9e3adff9728dacf69776013572a5
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-05 14:54:07 +0200

    Create various2.py

---
+++ strings/various2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# various2.py
+
+# hexadecimal
+print("{:x}".format(300))
+print("{:#x}".format(300))
+
+# binary
+print("{:b}".format(300))
+
+# octal
+print("{:o}".format(300))
+
+# scientific
+print("{:e}".format(300000))
