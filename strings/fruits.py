726aa9fd34749cad989f1f90d49f353a05035343
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-05 13:12:25 +0200

    Create fruits.py

---
+++ strings/fruits.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# fruits.py
+
+print ('There are %d oranges and %d apples in the basket' % (12, 23))
+print ('There are {0} oranges and {1} apples in the basket'.format(12, 23))
