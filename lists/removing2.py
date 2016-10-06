85c8fa8214b66a7c9859c3f133379e2a4bc69f63
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-06 17:11:07 +0200

    Create removing2.py

---
+++ lists/removing2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# removing2.py
+
+langs = ["Python", "Ruby", "Perl", "Lua", "JavaScript"]
+print (langs)
+
+del langs[1]
+print (langs)
+
+#del langs[15]
+
+del langs[:]
+print (langs)
