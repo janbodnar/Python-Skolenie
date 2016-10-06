fd8bb3be3cf021b7e6b3e3d23d4a3e321c9d0264
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-06 17:09:29 +0200

    Create removing.py

---
+++ lists/removing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# removing.py
+
+langs = ["Python", "Ruby", "Perl", "Lua", "JavaScript"]
+print (langs)
+
+langs.pop(3)
+langs.pop()
+print (langs)
+
+langs.remove("Ruby")
+print (langs)
