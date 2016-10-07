f026105a0f0e3b0c2b09a11b1c3892404cb023c2
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-07 12:28:55 +0200

    Create deep_copy.py

---
+++ lists/deep_copy.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# deep_copy.py
+
+import copy
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [a, b]
+
+d = copy.copy(c)
+
+print(id(d[0]) == id(c[0]))
+
+a[0] = 11
+print (d[0][0])
+
+
+print ("**********************")
+
+
+e = copy.deepcopy(c)
+
+print(id(e[0]) == id(c[0]))
+
+a[1] = 22
+print (e[0][1])
+print (e[0])
