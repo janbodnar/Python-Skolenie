75c8736631b66baaabe6eed9f4173b2488864f1f
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-07 13:28:32 +0200

    Create nested.py

---
+++ lists/nested.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# nested.py
+
+nums = [[1, 2], [3, 4], [5, 6]]
+
+print (nums[0])
+print (nums[1])
+print (nums[2])
+
+print (nums[0][0])
+print (nums[0][1])
+
+print (nums[1][0])
+print (nums[2][1])
+
+print (len(nums))
