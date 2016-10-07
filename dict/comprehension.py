c422b098f3d9d73b80efaf1d0d709648f5e0f4a5
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-07 17:34:13 +0200

    Create comprehension.py

---
+++ dict/comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# comprehension.py
+
+capitals = { "Bratislava": 424207, "Vilnius": 556723, "Lisbon": 564657,
+             "Riga": 713016, "Jerusalem": 780200, "Warsaw": 1711324,
+             "Budapest": 1729040, "Prague": 1241664, "Helsinki": 596661,
+             "Yokyo": 13189000, "Madrid": 3233527 }
+
+
+capitals2 = { key:val for key, val in capitals.items() if val < 1000000 }
+
+print (capitals2)
+
+
