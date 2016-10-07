bd0fdf3da213feb26e74fd64b0299f2716aecc5e
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-07 12:52:05 +0200

    Create indexing2.py

---
+++ lists/indexing2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# indexing2.py
+
+n = [1, 2, 3, 4, 1, 2, 3, 1, 2]
+
+print (n.index(1))
+print (n.index(2))
+
+print (n.index(1, 1))
+print (n.index(2, 2))
+
+print (n.index(1, 2, 5))
+print (n.index(3, 4, 8))
