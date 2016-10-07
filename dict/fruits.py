caad3d16d901288fa6ffdef248f6d2359fbf3bc9
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-07 17:55:31 +0200

    Create fruits.py

---
+++ dict/fruits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# fruits.py
+
+basket = ('oranges', 'pears', 'apples', 'bananas')
+
+fruits = {}.fromkeys(basket, 0)
+print (fruits)
+
+fruits['oranges'] = 12
+fruits['pears'] = 8
+fruits['apples'] = 4
+
+print (fruits.setdefault('oranges', 11))
+print (fruits.setdefault('kiwis', 11))
+
+print (fruits)
