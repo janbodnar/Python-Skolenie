787cfe3635b06ca9a8bca45fdfa5d1f3651c2149
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-07 17:57:41 +0200

    Create domains.py

---
+++ dict/domains.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# domains.py
+
+domains = { "de": "Germany", "sk": "Slovakia", "hu": "Hungary"}
+domains2 = { "us": "United States", "no": "Norway" }
+
+domains.update(domains2)
+
+print (domains)
