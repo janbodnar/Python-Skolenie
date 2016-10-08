c12fe442f7777f1e6f6f03f073833bdac4f48d0f
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 15:37:27 +0200

    Create object_identity.py

---
+++ oper/object_identity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# object_identity.py
+
+print (None == None)
+print (None is None)
+
+print (True is True)
+
+print ([] == [])
+print ([] is [])
+
+print ("Python" is "Python")
