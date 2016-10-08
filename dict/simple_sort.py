84535f94289e845bd7fe54a4dfd9da0c46898d36
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 11:31:22 +0200

    Create simple_sort.py

---
+++ dict/simple_sort.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# simple_sort.py
+
+items = { "coins": 7, "pens": 3, "cups": 2, 
+    "bags": 1, "bottles": 4, "books": 5 }
+    
+kitems = list(items.keys())
+kitems.sort()
+
+for k in kitems:
+    print (": ".join((k, str(items[k]))))
