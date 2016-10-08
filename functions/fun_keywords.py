e077b1facaad34a20d554606a8142c6a9869ff95
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 22:04:47 +0200

    Create fun_keywords.py

---
+++ functions/fun_keywords.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# fun_keywords.py
+
+def display(name, age, sex):
+
+   print ("Name: ", name)
+   print ("Age: ", age)
+   print ("Sex: ", sex)
+
+
+display("Lary", 43, "M")
+display("Joan", 24, "F")
