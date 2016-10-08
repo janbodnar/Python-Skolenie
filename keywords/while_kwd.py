71eca28a0dc1738ef6dbd03e98eebd72c56b1762
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 16:07:12 +0200

    Create while_kwd.py

---
+++ keywords/while_kwd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# while_kwd.py
+
+numbers = [22, 34, 12, 32, 4]
+sum = 0
+
+i = len(numbers)
+
+while (i != 0):
+    
+   i -= 1
+   sum = sum + numbers[i]
+
+print ("The sum is:", sum)
