d0006a3e0aba1712509c4dec0821aa688a009dc4
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 19:09:15 +0200

    Create del_kwd.py

---
+++ keywords/del_kwd.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# del_kwd.py
+
+class Rock:
+    
+    def __init__(self):
+        
+        self.weight = 4.5
+        self.col = "gray"
+        
+    def __str__(self):
+        
+        attr = []
+        
+        for key,value in self.__dict__.iteritems():
+            attr.append((key, value))
+            
+        return str(attr)    
+        
+rock = Rock()
+print rock
+
+del rock.col
+print rock
+
+a = [1, 2, 3, 4]
+
+print a
+del a[:2]
+print a
