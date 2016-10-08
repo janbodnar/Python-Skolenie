8ee51e5d90c93e59d6ea3440d34e070d806de60d
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 21:44:48 +0200

    Create returning2.py

---
+++ functions/returning2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# returning2.py
+
+n = [1, 2, 3, 4, 5]
+
+def stats(x):
+
+    mx = max(x)
+    mn = min(x)
+    ln = len(x)
+    sm = sum(x)
+    
+    return mx, mn, ln, sm    
+    
+mx, mn, ln, sm = stats(n)
+print (stats(n))
+
+print (mx, mn, ln, sm)
