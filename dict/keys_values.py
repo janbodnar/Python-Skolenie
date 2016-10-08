231fd789e35d059028a94d6e732355835d4f146c
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 11:25:41 +0200

    Create keys_values.py

---
+++ dict/keys_values.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# keys_values.py
+
+domains = { "de": "Germany", "sk": "Slovakia", "hu": "Hungary",
+    "us": "United States", "no": "Norway"  }
+
+print (domains.keys())
+print (domains.values())
+print (domains.items())
+
+print ("de" in domains)
+print ("cz" in domains)
+
+
