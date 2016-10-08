e55714067472577a33fd62759694074e657b586f
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 16:01:46 +0200

    Create tutorial.py

---
+++ keywords/tutorial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# tutorial.py
+
+print ("*" * 24)
+print ("*" * 24)
+print ("*" * 24)
+print ()
+print ("\tZetCode")
+print ()
+print ("*" * 24)
+print ("*" * 24)
+print ("*" * 24)
