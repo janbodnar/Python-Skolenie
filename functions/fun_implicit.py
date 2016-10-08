b9e3c0006caabbfcf8e621d3fdb21ec6607cdea2
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 21:59:08 +0200

    Create fun_implicit.py

---
+++ functions/fun_implicit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# fun_implicit.py
+
+def power(x, y=2):
+
+    r = 1
+    
+    for i in range(y):
+       r = r * x
+       
+    return r
+
+print (power(3))
+print (power(3, 3))
+print (power(5, 5))
