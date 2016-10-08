2d5484cc7c657be2a9195fdd9ea55e25e1793295
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 21:06:25 +0200

    Create returning.py

---
+++ functions/returning.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# returning.py
+
+def showMessage(msg):
+    
+    print msg
+
+def cube(x):
+    
+    return x * x * x   
+   
+x = cube(3)    
+print (x)
+
+showMessage("Computation finished.")
+print (showMessage("Ready."))
