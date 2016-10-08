20d723404709eff56e1ca1dd030345180f8a06d8
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 12:13:02 +0200

    Create arithmetic.py

---
+++ oper/arithmetic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# arithmetic.py
+
+a = 10
+b = 11
+c = 12
+
+add = a + b + c
+sub = c - a
+mult = a * b
+div = c / 3
+
+power = (a ** 2)
+
+print (add, sub, mult, div)
+print (power)
