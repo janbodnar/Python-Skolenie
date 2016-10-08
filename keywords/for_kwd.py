48e57746a0241f219cc61973dd7018afcf7c59b5
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 17:17:38 +0200

    Create for_kwd.py

---
+++ keywords/for_kwd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# for_kwd.py
+
+lyrics = """\
+Are you really here or am I dreaming
+I can't tell dreams from truth 
+for it's been so long since I have seen you
+I can hardly remember your face anymore 
+"""
+
+
+for i in lyrics:
+   print (i, end=" ")
