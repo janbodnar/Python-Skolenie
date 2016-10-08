d5207f7751a05f4f4372265c7429b76176785f1d
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 20:36:23 +0200

    Create ret.py

---
+++ functions/ret.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+The ret.py script shows how to work with
+functions in Python. 
+Author: Jan Bodnar
+ZetCode, 2016
+"""
+
+def showModuleName():
+    
+    print (__doc__)
+
+def getModuleFile():
+    
+   return (__file__)
+
+a = showModuleName()
+b = getModuleFile()
+
+print (a, b)
