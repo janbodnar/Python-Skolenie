cd2820475388ded3d9ac33cb8265530c4c2a75b0
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 16:08:48 +0200

    Create break_kwd.py

---
+++ keywords/break_kwd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# break_kwd.py
+
+import random
+
+while (True):
+
+   val = random.randint(1, 30)
+   print (val, end=" ")
+   
+   if (val ==  22):
+      break
+  
+print()  
