6b9b4cb955343e17b18a0781165919b899213312
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 11:44:38 +0200

    Create sorting2.py

---
+++ dict/sorting2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# sorting2.py
+
+items = { "coins": 7, "pens": 3, "cups": 2, 
+    "bags": 1, "bottles": 4, "books": 5 }
+    
+for key, value in sorted(items.items(), key=lambda val: val[1]):
+        
+    print ("%s: %s" % (key, value))
+
+print ("####### #######")
+    
+for key, value in sorted(items.items(), key=lambda val: val[1], reverse=True):
+         
+    print ("%s: %s" % (key, value))
