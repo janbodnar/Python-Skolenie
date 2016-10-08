09dc00eaf5956091bb096072c1f4e0e7c15e7bec
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-08 11:52:07 +0200

    Update sorting2.py

--- dict/sorting2.py
+++ dict/sorting2.py
@@ -5,12 +5,12 @@
 items = { "coins": 7, "pens": 3, "cups": 2, 
     "bags": 1, "bottles": 4, "books": 5 }
     
-for key, value in sorted(items.items(), key=lambda val: val[1]):
+for key, value in sorted(items.items(), key=lambda pair: pair[1]):
         
     print ("%s: %s" % (key, value))
 
 print ("####### #######")
     
-for key, value in sorted(items.items(), key=lambda val: val[1], reverse=True):
+for key, value in sorted(items.items(), key=lambda pair: pair[1], reverse=True):
          
     print ("%s: %s" % (key, value))
