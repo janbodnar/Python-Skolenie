9926898fb720d9fda8293123889411cd97325fd8
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-09 18:14:09 +0200

    Create inheritance.py

---
+++ oop/inheritance.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# inheritance.py
+
+class Animal(object):
+
+   def __init__(self):
+      print ("Animal created")
+
+   def whoAmI(self):
+      print ("Animal")
+
+   def eat(self):
+      print ("Eating")
+
+
+class Dog(Animal):
+
+   def __init__(self):
+      Animal.__init__(self)
+      print ("Dog created")
+
+   def whoAmI(self):
+      print ("Dog")
+
+   def bark(self):
+      print ("Woof!")
+
+d = Dog()
+d.whoAmI()
+d.eat()
+d.bark()
