2ece77e7495d2bdac60ab9d05421b209dd0617bb
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-09 16:59:46 +0200

    Create object_types.py

---
+++ oop/object_types.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# object_types.py
+
+import sys
+
+def function(): pass
+
+print (type(1))
+print (type(""))
+print (type([]))
+print (type({}))
+print (type(()))
+print (type(object))
+print (type(function))
+print (type(sys))
