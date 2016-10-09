1cd794dce95919f6c32492919569f9f79a2970ee
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-09 10:54:29 +0200

    Create arbitrary_args.py

---
+++ functions/arbitrary_args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# arbitrary_args.py
+
+def sum(*args):
+   '''Function returns the sum 
+   of all values'''
+   
+   r = 0
+   
+   for i in args:
+      r += i
+      
+   return r
+
+
+print (sum.__doc__)
+print (sum(1, 2, 3))
+print (sum(1, 2, 3, 4, 5))
