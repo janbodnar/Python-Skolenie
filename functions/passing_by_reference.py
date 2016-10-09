24df59637b1d972b3f5035a95158a7d06786ea85
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-09 11:10:05 +0200

    Create passing_by_reference.py

---
+++ functions/passing_by_reference.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# passing_by_reference.py
+
+n = [1, 2, 3, 4, 5]
+
+print ("Original list:", n)
+
+def f(x):
+
+    x.pop()
+    x.pop()
+    x.insert(0, 0)
+    print ("Inside f():", x)    
+    
+f(n)    
+
+print ("After function call:", n)
