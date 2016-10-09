3ac6feca413ff4b48ea0818207d0927d06c5c625
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-09 17:27:10 +0200

    Create attributes.py

---
+++ oop/attributes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# attributes.py
+
+class Cat:
+
+   def __init__(self, name):
+      self.name = name
+
+missy = Cat('Missy')
+lucky = Cat('Lucky')
+
+print (missy.name)
+print (lucky.name)
