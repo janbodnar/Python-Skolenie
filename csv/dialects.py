78bed54cb6da290d2cfdbde029979e63b2a5046f
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-10 19:54:10 +0200

    Create dialects.py

---
+++ csv/dialects.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# dialects.py
+
+import csv
+
+names = csv.list_dialects()
+
+for name in names:
+    
+    print(name)
+    
+    dialect = csv.get_dialect(name)
+    
+    print(repr(dialect.delimiter), end=" ")
+    print(dialect.doublequote, end=" ")
+    print(dialect.escapechar, end=" ")
+    print(repr(dialect.lineterminator), end=" ")
+    print(dialect.quotechar, end=" ")
+    print(dialect.quoting, end=" ")
+    print(dialect.skipinitialspace, end=" ")
+    print(dialect.strict)
