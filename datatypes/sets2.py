8420c504b784aa200058c5c0b4e71657ded1fac5
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-10 18:58:40 +0200

    Create sets2.py

---
+++ datatypes/sets2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# sets2.py
+
+set1 = set([1, 2])
+set1.add(3)
+set1.add(4)
+
+set2 = set([1, 2, 3, 4, 6, 7, 8])
+set2.remove(8)
+
+print (set1)
+print (set2)
+
+print ("Is set1 subset of set2 ? :", set1.issubset(set2))
+print ("Is set1 superset of set2 ? :", set1.issuperset(set2))
+
+set1.clear()
+print (set1)
