e7e4c9681d7f51979307a88890bf5417efd0ff43
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-10 19:49:21 +0200

    Create write_csv.py

---
+++ csv/write_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# write_csv.py
+
+import csv
+
+nms = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]]
+
+f = open('numbers2.csv', 'w')
+
+with f:
+
+    writer = csv.writer(f)
+    
+    for row in nms:
+        writer.writerow(row)
