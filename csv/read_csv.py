fb6f158a84ea09a81967047c98cf2c46492655cd
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-10 19:47:07 +0200

    Update and rename read.csv to read_csv.py

--- csv/read_csv.py
+++ csv/read_csv.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# read_csv.py
+
 import csv
 
 f = open('numbers.csv', 'r')
@@ -7,5 +9,6 @@ f = open('numbers.csv', 'r')
 with f:
 
     reader = csv.reader(f)
+    
     for row in reader:
         print(row)
