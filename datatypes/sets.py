bc8af13d43fb2838093aed03084e0a07289df55f
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-10 18:58:20 +0200

    Create sets.py

---
+++ datatypes/sets.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+set1 = set(['a', 'b', 'c', 'c', 'd'])
+set2 = set(['a', 'b', 'x', 'y', 'z'])
+
+print ("set1: " , set1)
+print ("set2: " , set2)
+print ("intersection: ", set1 & set2)
+print ("union: ", set1 | set2)
+print ("difference: ", set1 - set2)
+print ("symmetric difference: ", set1 ^ set2)
