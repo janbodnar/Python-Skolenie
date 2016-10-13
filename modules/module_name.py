b3c99e5e29c89ae7f54b7e9bb60fe74ca907f7a8
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-13 07:03:03 +0200

    Create module_name.py

---
+++ modules/module_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# module_name.py
+
+import empty
+import sys
+
+def main():
+
+    print (__name__)
+    print (empty.__name__)
+    print (empty.__doc__)
+    print (sys.__name__)
+
+if __name__ == '__main__':
+    main()
