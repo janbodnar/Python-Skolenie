cc433a1fcee1408167ddb95b381b2aee6aadce78
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-13 07:24:41 +0200

    Create my_math.py

---
+++ modules/my_math.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/python3
+
+"""
+A module containing the fibonacci
+function.
+"""
+
+# my_math.py
+
+def fib(n):
+
+    all = []
+    a, b = 0, 1
+
+    while b < n:
+
+        #print (b, end=" ")
+        (a, b) = (b, a + b)
+        all.extend((a, b))
+
+    return all
+
+
+# testing
+
+if __name__ == '__main__':
+   print (fib(500))
