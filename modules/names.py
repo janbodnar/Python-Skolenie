c103ab776dffd911a285a53ac41ef0134afb45f6
Jan Bodnar <jan.bodnar@gmail.com>
2016-10-13 08:17:31 +0200

    Create names.py

---
+++ modules/names.py
@@ -0,0 +1,14 @@
+"""
+names is a test module
+"""
+
+_version = 1.0
+
+names = ["Paul", "Frank", "Jessica", "Thomas", "Katherine"]
+
+def show_names():
+   for i in names:
+      print (i)
+
+def _show_version():
+   print (_version)
