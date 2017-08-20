91683903276850f9f01f1e145e621a24af916e22
Jan Bodnar <jan.bodnar@gmail.com>
2017-08-20 21:41:33 +0200

    Create read_file.py

---
+++ files/read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# redirect.py
+
+import sys
+
+with open('output.txt', 'w') as f:
+    
+    sys.stdout = f
+
+    print('Lucien')
+    sys.stdout.write('Rastignac\n')
+    sys.stdout.writelines(['Camusot\n', 'Collin\n'])
+
+    sys.stdout = sys.__stdout__
+
+    print('Bianchon')
+    sys.stdout.write('Lambert\n')
