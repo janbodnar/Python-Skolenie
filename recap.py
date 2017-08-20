895985c9889bc40877bf6755d9696728b7a7cecc
Jan Bodnar <jan.bodnar@gmail.com>
2017-08-20 23:04:55 +0200

    Create recap.py

---
+++ recap.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+
+# recapitulation
+# recap.py
+
+import random
+import os
+
+# ******************************      
+# os module
+
+print(os.getcwd())
+
+try:
+
+    os.mkdir('newfolder')
+    
+except FileExistsError as e:
+    print(e)
+
+print("******************************") 
+
+# ******************************      
+# list, for loop, built-ins
+
+
+nums = [1, 2, 3, 4, 5]
+
+print("There are {0} elements in the list".format(len(nums)))
+
+for e in nums:
+    print(e, end=' ')
+    
+print()    
+
+print("maximum: {0}".format(max(nums)))
+print("maximum: {0}".format(max(nums)))
+print("summation: {0}".format(sum(nums)))
+
+
+# ******************************      
+# while loop
+
+male = False
+male = bool(random.randint(0, 1))
+
+if (male):
+   print("We will use name John")
+else:
+   print("We will use name Victoria")
+   
+ 
+print("******************************") 
+
+# ******************************      
+# dictionary   
+
+weekend = { "Sun": "Sunday", "Mon": "Monday" }
+vals = dict(one=1, two=2)
+
+capitals = {}
+capitals["svk"] = "Bratislava"
+capitals["deu"] = "Berlin"
+capitals["dnk"] = "Copenhagen"
+
+d = { i: object() for i in range(4) }
+
+print(weekend)
+print(vals)
+print(capitals)
+print(d)
+
+for k, v in weekend.items():
+    print("key: {0}, value: {1}".format(k, v))
+
+print("******************************")
+
+# ******************************      
+# while loop
+
+i = len(nums) - 1
+mysum = 0
+
+while (i >= 0):
+    
+    mysum += nums[i]
+    i -= 1
+    
+
+print("summation 2: {0}".format(mysum))
+
+print("******************************")
+
+# ******************************      
+# sets
+
+set1 = { 'a', 'b', 'c', 'c', 'd' }
+set2 = { 'a', 'b', 'x', 'y', 'z' }
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("intersection:", set1.intersection(set2))
+print("union:", set1.union(set2))
+print("difference:", set1.difference(set2))
+print("symmetric difference:", set1.symmetric_difference(set2))
