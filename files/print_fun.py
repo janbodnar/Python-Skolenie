eaac8e1ad08f9a1851840835a6ffb9dda1d30608
Jan Bodnar <jan.bodnar@gmail.com>
2017-08-20 21:09:50 +0200

    Create print_fun.py

---
+++ files/print_fun.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# print_fun.py
+
+print('Honore de Balzac')
+print('The Splendors and Miseries of Courtesans', 'Gobseck', 'Father Goriot', sep=":")
+
+vals = [1, 2, 3, 4, 5]
+
+for e in vals:
+    print(e, end=' ')
+
+print()   
