ff70b1fb80c82c7a4b817777cee3ba8752b015a4
Jan Bodnar <jan.bodnar@gmail.com>
2017-08-20 21:41:14 +0200

    Create strophe.py

---
+++ files/strophe.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# strophe.py
+
+text = '''Incompatible, it don't matter though
+'cos someone's bound to hear my cry
+Speak out if you do
+You're not easy to find\n'''
+
+with open('strophe.txt', 'w') as f:
+    f.write(text)
