5983365231ccbdbdcbff81e2ec80e93c2975628e
Jan Bodnar <jan.bodnar@gmail.com>
2017-08-20 20:54:32 +0200

    Create read_name.py

---
+++ files/read_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# read_name.py
+
+import sys
+
+print('Enter your name: ', end='')
+
+name = ''
+
+sys.stdout.flush()
+
+while True:
+    
+   c = sys.stdin.read(1)
+   
+   if c == '\n':
+      break
+  
+   name = name + c
+
+print('Your name is:', name)
