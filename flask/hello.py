a710c3cfc95ab36ba1ed4d418cc8d23174fd67b6
Jan Bodnar <jan.bodnar@gmail.com>
2017-08-22 08:28:35 +0200

    Create hello.py

---
+++ flask/hello.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+    # return "Hello World!"
+
+@app.route("/")	
+def home():
+    return app.send_static_file('index.html')
+	
+# @app.route("/hello/<name>")
+# def greet(name):
+    # return "Hello {0}".format(name)
+	
+# @app.route("/hello/<name>")
+# def greet(name):
+    # return render_template('index.html', name=name)
+	
+@app.route("/greet")
+def greet():
+    username = request.args.get('name')
+    return render_template('index.html', name=username)
+	
+if __name__ == "__main__":
+    app.run()
