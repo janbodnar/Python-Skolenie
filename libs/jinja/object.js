c4a9262603a1e618a69656edac94a939c7d50cf2
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-13 07:11:56 +0200

    Create object.js

---
+++ libs/jinja/object.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from jinja2 import Template
+
+class Person:
+
+    def __init__(self, name, age):
+
+        self.name = name
+        self.age = age
+
+    def getAge(self):
+        return self.age
+
+    def getName(self):
+        return self.name    
+
+
+person = Person('Peter', 34)
+
+tm = Template("My name is {{ per.getName() }} and I am {{ per.getAge()}}")
+msg = tm.render(per=person)
+
+print(msg)
