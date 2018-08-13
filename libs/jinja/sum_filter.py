42638c090b19b81371d00de35eb5916f3e50a9a2
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-13 07:07:58 +0200

    Create sum_filter.py

---
+++ libs/jinja/sum_filter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from jinja2 import Environment, FileSystemLoader
+
+cars = [
+    {'name': 'Audi', 'price': 23000}, 
+    {'name': 'Skoda', 'price': 17300}, 
+    {'name': 'Volvo', 'price': 44300}, 
+    {'name': 'Volkswagen', 'price': 21300}
+]
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+
+template = env.get_template('sumprices.txt')
+
+output = template.render(cars=cars)
+print(output)
