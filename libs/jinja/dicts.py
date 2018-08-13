f6b8d5d32af284450ec0a060bfad97f54341ab79
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-13 07:10:45 +0200

    Create dicts.py

---
+++ libs/jinja/dicts.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from jinja2 import Template
+
+person = { 'name': 'Person', 'age': 34 }
+
+tm = Template("My name is {{ per.name }} and I am {{ per.age }}")
+msg = tm.render(per=person)
+
+print(msg)
