efebca3fca432a72e3b081adba39745ad2fcecc1
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-14 18:06:23 +0200

    Create list_files.py

---
+++ os/list_files.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os
+
+files = filter(os.path.isfile, os.listdir(os.curdir))
+
+for myfile in files:
+    print(myfile)
+
+# files = [myfile for myfile in os.listdir('.') if os.path.isfile(os.path.join('.', myfile))]
+# print(files)
+
+# for fname in os.listdir('.'):
+#     if os.path.isdir(fname):
+#        pass  
+#     else:
+#        pass  
