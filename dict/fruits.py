a74cb84ce9b65fcca97c374d891a83175459333c
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-14 21:18:44 +0200

    Update fruits.py

--- dict/fruits.py
+++ dict/fruits.py
@@ -5,13 +5,13 @@
 basket = ('oranges', 'pears', 'apples', 'bananas')
 
 fruits = {}.fromkeys(basket, 0)
-print (fruits)
+print(fruits)
 
 fruits['oranges'] = 12
 fruits['pears'] = 8
 fruits['apples'] = 4
 
-print (fruits.setdefault('oranges', 11))
-print (fruits.setdefault('kiwis', 11))
+print(fruits.setdefault('oranges', 11))
+print(fruits.setdefault('kiwis', 11))
 
-print (fruits)
+print(fruits)
