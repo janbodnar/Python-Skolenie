c2c18eeb29f82ae328ceebb50a950bb77993c0fa
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-14 08:28:00 +0200

    Create read_image.py

---
+++ io/read_image.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import binascii
+from more_itertools import sliced
+
+# sudo pip3 install more_itertools
+
+with open('sid.jpg', 'rb') as f:
+
+    content = f.read()
+    hexed = binascii.hexlify(content)
+    # print(hexed)
+    mybytes = list(sliced(hexed, 2))
+
+    i = 0
+    for b in mybytes:
+        print(b.decode("utf-8") , end=' ')
+        i += 1
+        if (i % 30 == 0):
+            print()
