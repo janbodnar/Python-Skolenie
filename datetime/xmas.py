e7565f5e67aecfa9266007224d0e443e6950233b
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-15 23:23:39 +0200

    Create xmas.py

---
+++ datetime/xmas.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+
+now = datetime.now()
+xmas = datetime(now.year, 12, 25)
+delta = xmas - now
+final = delta.days
+
+if final > 0:
+    print(final, "days until XMas")
+elif final == 0:
+    print("Merry XMas!")
+elif final < 0:
+    print("It is past XMas, wait until next year!")
