bf087699ff96d1bd9695b0b46ed807dc629b0bce
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-15 07:44:42 +0200

    Create disk_usage.py

---
+++ os/disk_usage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import shutil
+
+total_b, used_b, free_b = shutil.disk_usage('.')
+
+gb = 10 ** 9 # GB == gigabyte
+
+print('Total: {:6.2f} GB'.format(total_b / gb))
+print('Used : {:6.2f} GB'.format(used_b / gb))
+print('Free : {:6.2f} GB'.format(free_b / gb))
