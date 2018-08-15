9097191fb6847883476a9999b30cfa73f0cc62c7
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-15 22:36:24 +0200

    Create extract_html.py

---
+++ new/extract_html.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import re
+
+content = '''<p>The <code>pattern</code> is a compiled representation of 
+a regular expression.</p>'''
+
+pattern = re.compile(r'</?[a-z]*>')
+
+matches = re.findall(pattern, content)
+
+for match in matches:
+    print(match)
