6b1889e88742c211c6978aac3fc0d58b008e4317
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-15 23:12:24 +0200

    Create today_now.py

---
+++ datetime/today_now.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import datetime
+import time
+
+now = datetime.datetime.now()
+print(now)
+
+d = datetime.date.today()
+print(d)
+
+t = datetime.datetime.now().time()
+print(t)
