e4dbcf6be00ae6666b647b186c098e9254ef82ed
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-15 23:20:35 +0200

    Create time_deltas.py

---
+++ datetime/time_deltas.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import datetime
+
+now = datetime.datetime.now()
+
+print(now)
+
+dt1 = now + datetime.timedelta(days=1, hours=2)
+print(dt1)
+
+dt2 = now - datetime.timedelta(weeks=9)
+print(dt2)
