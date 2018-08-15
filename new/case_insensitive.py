e72022a1b30f6fb1a99ce6e42a9ac27cf5eb4a1a
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-15 22:29:52 +0200

    Create case_insensitive.py

---
+++ new/case_insensitive.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import re
+
+words = ('dog', 'Dog', 'DOG', 'Doggy')
+
+pattern = re.compile(r'^dog', re.IGNORECASE)
+
+for word in words:
+    if re.match(pattern, word):
+        print('{} matches'.format(word))
