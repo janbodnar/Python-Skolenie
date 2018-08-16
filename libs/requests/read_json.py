11b7b8a3ba9bbeaccbe65e9676fe08930a10c30d
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-16 08:08:51 +0200

    Create read_json.py

---
+++ libs/requests/read_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import requests as req
+
+resp = req.get('https://jsonplaceholder.typicode.com/posts')
+json_list = resp.json()
+
+for e in json_list:
+    print('Id: {}'.format(e['id']))
+    print('Title: {}'.format(e['title']))
+    print('Body: {}'.format(e['body']))
