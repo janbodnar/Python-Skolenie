34b06fb95b8706745dcb1277c95e94d8c30ac44b
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-16 07:48:40 +0200

    Create parsing.py

---
+++ datetime/parsing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+
+dt1 = datetime.strptime('Jun 1 2018 5:33PM', '%b %d %Y %I:%M%p')
+print(dt1)
+
+dt2 = datetime.strptime('23:33:45', '%H:%M:%S')
+print(dt2.time())
