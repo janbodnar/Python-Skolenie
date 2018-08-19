8312f9d495d107840d9b584ca95406554ad7850d
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-19 21:30:23 +0200

    Create question_mark_meta.py

---
+++ stdlib/re/question_mark_meta.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import re
+
+words = ('seven', 'even','prevent', 'revenge', 'maven', 
+    'eleven', 'amen', 'event')
+
+pattern = re.compile(r'.?even')
+
+for word in words:
+    if re.match(pattern, word):
+        print('The {} matches '.format(word))
