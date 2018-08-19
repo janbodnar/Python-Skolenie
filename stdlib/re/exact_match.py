2da5810d1c811cf31207627739cc901d73fbe6d4
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-19 21:26:18 +0200

    Create exact_match.py

---
+++ stdlib/re/exact_match.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import re
+
+# we can use fullmatch() or ^ $ characters
+
+words = ('book', 'bookworm', 'Bible', 
+    'bookish','cookbook', 'bookstore', 'pocketbook')
+
+pattern = re.compile(r'^book$')
+
+for word in words:
+    if re.search(pattern, word):
+        print('The {} matches'.format(word))    
