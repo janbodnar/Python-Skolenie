e33ae7026ed93cb24ae5fc88f700ca3a6d7af17f
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-20 00:54:26 +0200

    Update case_insensitive.py

--- stdlib/re/case_insensitive.py
+++ stdlib/re/case_insensitive.py
@@ -4,7 +4,7 @@ import re
 
 words = ('dog', 'Dog', 'DOG', 'Doggy')
 
-pattern = re.compile(r'^dog', re.IGNORECASE)
+pattern = re.compile(r'dog', re.IGNORECASE)
 
 for word in words:
     if re.match(pattern, word):
