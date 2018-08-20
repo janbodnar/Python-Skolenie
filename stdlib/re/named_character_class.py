2c387bc93f134c6fc802e0077b18c5d5989a2592
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-20 12:35:43 +0200

    Create named_character_class.py

---
+++ stdlib/re/named_character_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import re
+
+text = 'We met in 2013. She must be now about 27 years old.'
+
+pattern = re.compile(r'\d+')
+
+found = re.findall(pattern, text)
+
+if found:
+    print('There are {} numbers'.format(len(found)))    
