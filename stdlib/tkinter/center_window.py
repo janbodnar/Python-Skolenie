04a89b9a2629e30f043624e6b680d1a582c0c979
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:42:36 +0200

    Create center_window.py

---
+++ stdlib/tkinter/center_window.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+root = tkinter.Tk()
+root.title('Centered window')
+
+win_width = 300
+win_height = 250 
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+start_x = int((screen_width/2) - (win_width/2))
+start_y = int((screen_height/2) - (win_height/2))
+
+root.geometry('{}x{}+{}+{}'.format(win_width, win_height, 
+    start_x, start_y))
+root.mainloop()
