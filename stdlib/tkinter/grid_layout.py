5111dacef1dc01328252c8bcc40ae3c84d541b1f
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:45:39 +0200

    Create grid_layout.py

---
+++ stdlib/tkinter/grid_layout.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+root = tkinter.Tk()
+root.title('Grid')
+
+root.columnconfigure(0, pad=5)        
+root.columnconfigure(1, pad=5) 
+root.columnconfigure(2, pad=5) 
+root.rowconfigure(0, pad=5)
+
+btn1 = tkinter.Button(root, text='Button 1')
+btn1.grid(column=0, row=0)
+
+btn2 = tkinter.Button(root, text='Button 2')
+btn2.grid(column=1, row=0)
+
+btn3 = tkinter.Button(root, text='Button 3')
+btn3.grid(column=2, row=0)
+
+btn4 = tkinter.Button(root, text='Button 4')
+btn4.grid(column=0, row=1)
+
+btn5 = tkinter.Button(root, text='Button 5')
+btn5.grid(column=1, row=1)
+
+btn6 = tkinter.Button(root, text='Button 6')
+btn6.grid(column=2, row=1)
+
+root.geometry('300x300+300+250')
+root.mainloop()
