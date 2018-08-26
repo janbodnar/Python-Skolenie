47fe86b9bba3bc7a7fb4ec19e36e495a1c7c0815
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 21:19:57 +0200

    Create last_inserted_rowid.py

---
+++ stdlib/sqlite3/last_inserted_rowid.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import sys
+
+con = sqlite3.connect(':memory:')
+
+with con:
+    
+    cur = con.cursor()    
+    cur.execute("CREATE TABLE friends(id INTEGER PRIMARY KEY, name TEXT)")
+    cur.execute("INSERT INTO friends(name) VALUES ('Tom')")
+    cur.execute("INSERT INTO friends(name) VALUES ('Rebecca')")
+    cur.execute("INSERT INTO friends(name) VALUES ('Jim')")
+    cur.execute("INSERT INTO friends(name) VALUES ('Robert')")
+        
+    lid = cur.lastrowid
+    print("The last Id of the inserted row is {}".format(lid))
