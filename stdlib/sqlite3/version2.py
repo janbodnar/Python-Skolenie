1035d34c976647928f526d364bf8b9d6ab152134
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 21:16:05 +0200

    Create version2.py

---
+++ stdlib/sqlite3/version2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import sys
+
+con = sqlite3.connect('test.db')
+
+with con:
+    
+    cur = con.cursor()    
+    cur.execute('SELECT SQLITE_VERSION()')
+    
+    version = cur.fetchone()
+    
+    print("SQLite version: {}".format(version[0]))
