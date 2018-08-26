8d0227badcfe3e236321db56a2e5271dce0424a7
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:42:51 +0200

    Create checkbutton_example.py

---
+++ stdlib/tkinter/checkbutton_example.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+def toggleTitle():
+
+    isTitleShown = cbvar.get()
+
+    if isTitleShown:
+        root.title('Checkbutton example')
+    else:
+        root.title('')
+
+root = tkinter.Tk()
+root.title('Checkbutton example')
+
+cbvar = tkinter.BooleanVar()
+cbtn = tkinter.Checkbutton(root, text="Show", width=8, variable=cbvar,
+    command=toggleTitle)
+cbtn.select()
+cbtn.pack()
+
+root.geometry("300x250+300+300")
+root.mainloop()
