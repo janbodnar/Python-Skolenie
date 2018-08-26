c8847904a096d598c835341425268031a73446a5
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:41:47 +0200

    Create canvas_colours.py

---
+++ stdlib/tkinter/canvas_colours.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+root = tkinter.Tk()
+root.title('Canvas colours')
+
+canvas = tkinter.Canvas(root)
+canvas.create_rectangle(30, 10, 120, 80, 
+    outline="#fb0", fill="#fb0")
+canvas.create_rectangle(150, 10, 240, 80, 
+    outline="#f50", fill="#f50")
+canvas.create_rectangle(270, 10, 370, 80, 
+    outline="#05f", fill="#05f")            
+canvas.pack(fill=tkinter.BOTH, expand=1)
+
+root.geometry("400x100+300+300")
+root.mainloop()
