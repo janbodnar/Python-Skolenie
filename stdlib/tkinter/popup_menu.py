7b57ffdb10b96fdab9a0486c8bfbb0e90a591b26
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:48:48 +0200

    Create popup_menu.py

---
+++ stdlib/tkinter/popup_menu.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import tkinter
+import tkinter.messagebox
+import datetime
+
+def showDay():
+    
+    now = datetime.datetime.now()
+    msg = 'Today is: {}'.format(now.strftime('%A'))
+    tkinter.messagebox.showinfo("Information", msg)
+
+def showMenu(e):
+
+    pmenu.post(e.x_root, e.y_root)
+
+root = tkinter.Tk()
+root.title('popup menu')
+
+pmenu = tkinter.Menu(root, tearoff=0)
+
+pmenu.add_command(label="Show day", command=showDay)
+pmenu.add_command(label="Exit", command=root.quit)
+
+root.bind("<Button-3>", showMenu)
+
+root.geometry('300x300+300+250')
+root.mainloop()
