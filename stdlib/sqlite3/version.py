6d685a79ec8355d114a3747701cadaf93da334e0
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 21:15:48 +0200

    Create version.py

---
+++ stdlib/sqlite3/version.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import sys
+
+con = None
+
+try:
+    con = sqlite3.connect('test.db')
+    
+    cur = con.cursor()    
+    cur.execute('SELECT SQLITE_VERSION()')
+    
+    version = cur.fetchone()
+    
+    print("SQLite version: {}".format(version[0])) 
+    
+except sqlite3.Error as e:
+    
+    print("Error {}:".format(e.args[0]))
+    sys.exit(1)
+    
+finally:
+    
+    if con:
+        con.close()
