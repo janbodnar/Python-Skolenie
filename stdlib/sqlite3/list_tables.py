f559ad0d9607872420e890953f18d9592019b55e
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 21:20:40 +0200

    Create list_tables.py

---
+++ stdlib/sqlite3/list_tables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import sys
+
+con = sqlite3.connect('test.db')
+
+with con:
+    
+    cur = con.cursor()    
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row[0])
