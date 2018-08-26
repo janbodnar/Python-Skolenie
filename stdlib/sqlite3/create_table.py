e5b1782175edebed0fe66fd7ca5e880a596e80c2
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 21:16:33 +0200

    Create create_table.py

---
+++ stdlib/sqlite3/create_table.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import sys
+
+con = sqlite3.connect('test.db')
+
+with con:
+    
+    cur = con.cursor()    
+    cur.execute("CREATE TABLE cities(id INTEGER PRIMARY KEY, name TEXT, population INT)")
+    cur.execute("INSERT INTO cities(name, population) VALUES('Bratislava', 432000)")
+
+    cur.execute("INSERT INTO cities(name, population) VALUES('Budapest', 1759000)")
+    cur.execute("INSERT INTO cities(name, population) VALUES('Prague', 1280000)")
+    cur.execute("INSERT INTO cities(name, population) VALUES('Warsaw', 1748000)")
+    cur.execute("INSERT INTO cities(name, population) VALUES('Los Angeles', 3971000)")
+    cur.execute("INSERT INTO cities(name, population) VALUES('New York', 8550000)")
+    cur.execute("INSERT INTO cities(name, population) VALUES('Edinburgh', 464000)")
+    cur.execute("INSERT INTO cities(name, population) VALUES('Suzhou', 4327066)")
+    cur.execute("INSERT INTO cities(name, population) VALUES('Zhengzhou', 4122087)")
+    cur.execute("INSERT INTO cities(name, population) VALUES('Berlin', 3671000)")
