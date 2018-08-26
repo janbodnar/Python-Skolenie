dcae4b41816cc8c2c7e0f7488f5e587423453d2c
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:48:23 +0200

    Create mouse_press_event.py

---
+++ stdlib/tkinter/mouse_press_event.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+def onButtonPress(e):
+
+    msg = 'x: {} y: {}'.format(e.x, e.y)
+    lvar.set(msg)
+
+root = tkinter.Tk()
+root.title('Mouse press event')
+
+lvar = tkinter.StringVar()
+lbl = tkinter.Label(root, textvariable=lvar)
+lbl.place(x=20, y=20)
+
+root.bind('<Button>', onButtonPress)
+
+root.geometry("300x250+300+300")
+root.mainloop()
