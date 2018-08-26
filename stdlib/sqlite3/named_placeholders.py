d4ae3d354a0a1fb5c97e51f63a0e85d2f6274e6a
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 21:21:37 +0200

    Create named_placeholders.py

---
+++ stdlib/sqlite3/named_placeholders.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import sys
+
+uid = 2
+
+con = sqlite3.connect('test.db')
+
+with con:
+
+    cur = con.cursor()    
+
+    cur.execute("SELECT name, population FROM cities WHERE id=:id", {"id": uid})        
+    con.commit()
+    
+    row = cur.fetchone()
+    print(row[0], row[1])
