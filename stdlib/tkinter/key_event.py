3e68fd05ba37f478417d9084d545169e3f2d221a
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:46:53 +0200

    Create key_event.py

---
+++ stdlib/tkinter/key_event.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+def onKeyPress(e):
+
+    msg = 'Key {}, (code {}) was pressed'.format(e.keysym, e.keycode)
+    lvar.set(msg)
+
+root = tkinter.Tk()
+root.title('Key event')
+
+lvar = tkinter.StringVar()
+lbl = tkinter.Label(root, text="coordinates", textvariable=lvar)
+lbl.place(x=20, y=20)
+
+root.bind('<Key>', onKeyPress)
+
+root.geometry("300x250+300+300")
+root.mainloop()
