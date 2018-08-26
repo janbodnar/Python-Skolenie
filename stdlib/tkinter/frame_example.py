77ac7ee93c37d46c5300c2adeadea5241cd03b27
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:45:04 +0200

    Create frame_example.py

---
+++ stdlib/tkinter/frame_example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+root = tkinter.Tk()
+root.title('Frame widget')
+
+frame = tkinter.Frame(root, borderwidth=15)
+
+btn = tkinter.Button(frame, text="Quit", padx=5, pady=5, width=10,
+    command=root.destroy)
+btn.pack()
+
+frame.pack()
+
+root.mainloop()
