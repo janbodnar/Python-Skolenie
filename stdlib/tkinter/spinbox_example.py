e9bc603f348bfc8f2bd4eff8f7cca430d0d52858
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:49:48 +0200

    Create spinbox_example.py

---
+++ stdlib/tkinter/spinbox_example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+def onSelect():
+    
+    val = spinbox.get()
+    lvar.set(val)
+
+root = tkinter.Tk()
+root.title('Spinbox example')
+
+root.columnconfigure(0, pad=5)        
+root.columnconfigure(1, pad=15) 
+root.rowconfigure(0, pad=5)
+
+spinbox = tkinter.Spinbox(root, from_=0, to=100, command=onSelect)
+spinbox.grid(column=0, row=0)    
+
+lvar = tkinter.StringVar()
+label = tkinter.Label(root, text=0, textvariable=lvar)    
+label.grid(column=1, row=0)
+
+root.geometry("300x250+300+300")
+root.mainloop()
