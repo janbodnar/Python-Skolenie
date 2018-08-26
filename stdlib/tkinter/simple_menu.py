e90a58727c82b8663a7480db3e2497a43662e46a
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:49:30 +0200

    Create simple_menu.py

---
+++ stdlib/tkinter/simple_menu.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import tkinter
+import tkinter.messagebox
+import datetime
+
+def showDay():
+    
+    now = datetime.datetime.now()
+    msg = 'Today is: {}'.format(now.strftime('%A'))
+    tkinter.messagebox.showinfo("Information", msg)
+
+root = tkinter.Tk()
+root.title('Menu')
+
+menubar = tkinter.Menu(root)
+root.config(menu=menubar)
+
+fileMenu = tkinter.Menu(menubar)
+
+fileMenu.add_command(label="Show day", command=showDay)
+fileMenu.add_command(label="Exit", command=root.quit)
+
+menubar.add_cascade(label="File", menu=fileMenu)
+
+root.geometry('300x300+300+250')
+root.mainloop()
