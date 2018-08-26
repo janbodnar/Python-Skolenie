6e56ca89abc7c918d1077b16e0bd533b0a8db3c5
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:49:19 +0200

    Create scale_example.py

---
+++ stdlib/tkinter/scale_example.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+def onScale(val):
+    
+    v = int(val)
+    lvar.set(v)
+
+root = tkinter.Tk()
+root.title('Scale example')
+
+root.columnconfigure(0, pad=5)        
+root.columnconfigure(1, pad=15) 
+root.rowconfigure(0, pad=5)
+
+scale = tkinter.Scale(root, from_=0, to=100, orient=tkinter.HORIZONTAL, 
+    command=onScale)
+scale.grid(column=0, row=0)    
+
+lvar = tkinter.IntVar()
+label = tkinter.Label(root, text=0, textvariable=lvar)    
+label.grid(column=1, row=0)
+
+root.geometry("300x250+300+300")
+root.mainloop()
