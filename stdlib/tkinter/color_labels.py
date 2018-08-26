31446cd72bcceb9df4924dfc3d1c91a24d45e65d
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:43:24 +0200

    Create color_labels.py

---
+++ stdlib/tkinter/color_labels.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+root = tkinter.Tk()
+
+l1 = tkinter.Label(root, text="simple app", 
+    padx=10, pady=10, fg="red")
+l1.pack()
+
+l2 = tkinter.Label(root, text="simple app", 
+    padx=10, pady=10, fg="blue")
+l2.pack()
+
+l2 = tkinter.Label(root, text="simple app", 
+    padx=10, pady=10, fg="brown")
+l2.pack()
+
+root.mainloop()
