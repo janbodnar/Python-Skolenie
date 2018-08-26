24689cee69d8560b70641278463b34613e64d91e
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-26 16:48:09 +0200

    Create mouse_move_event.py

---
+++ stdlib/tkinter/mouse_move_event.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import tkinter
+
+def onMotion(e):
+
+    msg = 'x: {} y: {}'.format(e.x, e.y)
+    lvar.set(msg)
+
+root = tkinter.Tk()
+root.title('Mouse move event')
+
+lvar = tkinter.StringVar()
+lbl = tkinter.Label(root, text="coordinates", textvariable=lvar)
+lbl.place(x=20, y=20)
+
+root.bind('<Motion>', onMotion)
+
+root.geometry("300x250+300+300")
+root.mainloop()
