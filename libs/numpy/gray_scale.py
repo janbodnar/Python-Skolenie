426fada8ba8f3c23d45700c11c01460dfff18d16
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-27 19:15:37 +0200

    Create gray_scale.py

---
+++ libs/numpy/gray_scale.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# formula
+# Y' = 0.299 R + 0.587 G + 0.114 B 
+
+def rgb2gray(rgb):
+
+    print(rgb[...,:3].shape)
+    # print(rgb.size)
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+img = mpimg.imread('sid.jpg')     
+gray_img = rgb2gray(img)    
+
+plt.imshow(gray_img, cmap=plt.get_cmap('gray'))
+plt.show()
