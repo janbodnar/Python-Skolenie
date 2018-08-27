f54db7fd4cde615a4677f0d3f3bf0ea7e450b53a
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-27 16:46:02 +0200

    Create simple.py

---
+++ libs/numpy/simple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+a = np.array([1, 2, 3])
+print(a)
+print("********************")
+
+b = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(b)
+print("********************")
+
+print(2*b)
+print("********************")
+
+print(a*b)
+print("********************")
