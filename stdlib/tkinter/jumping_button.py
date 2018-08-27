307ac9e10f6d024246887e39cb7f5e1a4f1fd8f5
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-27 14:41:25 +0200

    Create jumping_button.py

---
+++ stdlib/tkinter/jumping_button.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import tkinter
+import random
+
+def onEnter(e):
+
+    w = root.winfo_width()
+    h = root.winfo_height()
+
+    b_w = btn.winfo_width()
+    b_h = btn.winfo_height()
+
+    r_x = random.randrange(0, w - b_w)
+    r_y = random.randrange(0, h - b_h)
+
+    btn.place(x=r_x, y=r_y)
+
+
+root = tkinter.Tk()
+root.title('Mouse move event')
+root.resizable(False, False)
+
+btn = tkinter.Button(root, text='Exit', width=10, command=root.quit)
+btn.place(x=180, y=20)
+
+root.bind('<Enter>', onEnter)
+
+root.geometry("600x600+30+30")
+root.mainloop()
