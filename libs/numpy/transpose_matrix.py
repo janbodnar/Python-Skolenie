986c14d71ae34197313efbf546d43e305dc1d82c
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-27 17:00:05 +0200

    Create transpose_matrix.py

---
+++ libs/numpy/transpose_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+x = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(x)    
+print(x.T)  
+            
+print("**********************")            
+
+v = np.array([1,2,3])
+print(v)    
+print(v.T)  
