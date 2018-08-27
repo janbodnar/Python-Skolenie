978517da03fae5498f594a736ba1f750fe9eed6d
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-27 16:57:59 +0200

    Create summation.py

---
+++ libs/numpy/summation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+x = np.array([[1, 2, 3],[4, 5, 6], [7, 8, 9]])
+
+print(x)
+
+print(np.sum(x))  
+print(np.sum(x, axis=0))  
+print(np.sum(x, axis=1))  
