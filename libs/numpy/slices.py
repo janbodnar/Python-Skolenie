3031c22bfcb05a292fa93f40ca5f928c041c2ef1
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-27 19:17:21 +0200

    Create slices.py

---
+++ libs/numpy/slices.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+a = np.array([[ 1,  2,  3,  4],
+       [ 5,  6,  7,  8],
+       [ 9, 10, 11, 12],
+       [13, 14, 15, 16]])
+
+print(a[:2,:2])
+print(a[:3,:3])
+
+print("*********************")
+
+print(a[0,...])
+print(a[...,:3])
