ed7facb7a36582eca74974614ecae3d88b4bcded
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-28 09:01:11 +0200

    Update fruits.py

--- strings/fruits.py
+++ strings/fruits.py
@@ -2,5 +2,5 @@
 
 # fruits.py
 
-print ('There are %d oranges and %d apples in the basket' % (12, 23))
-print ('There are {0} oranges and {1} apples in the basket'.format(12, 23))
+print('There are %d oranges and %d apples in the basket' % (12, 23))
+print('There are {0} oranges and {1} apples in the basket'.format(12, 23))
