72edac700a23627eae2ef40d084bc23faab04752
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-28 08:30:14 +0200

    Create load_save.py

---
+++ libs/numpy/load_save.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+a = np.array([1,2,3,4,5]) 
+
+np.save('data', a)
+
+b = np.load('data.npy') 
+print(b)
+
+# a = np.array([1,2,3,4,5]) 
+# np.savetxt('data.txt',a) 
+
+# b = np.loadtxt('data.txt') 
+# print(b) 
