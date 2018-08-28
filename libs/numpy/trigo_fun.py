2d21072925d7a4e61729f1fd6f0170fc2ba6ffbf
Jan Bodnar <jan.bodnar@gmail.com>
2018-08-28 08:19:12 +0200

    Create trigo_fun.py

---
+++ libs/numpy/trigo_fun.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+a = np.array([0, 30, 45, 60, 90, 120]) 
+
+# Convert to radians by multiplying with pi/180 
+print(np.sin(a*np.pi/180))
+print(np.cos(a*np.pi/180))
+print(np.tan(a*np.pi/180)) 
