84d2289b446b078afac1e635d85fa732f170d339
Jan Bodnar <jan.bodnar@gmail.com>
2018-09-23 22:02:05 +0200

    Update quit_button.py

--- stdlib/tkinter/quit_button.py
+++ stdlib/tkinter/quit_button.py
@@ -8,7 +8,7 @@ root.title('Quit button')
 
 btn = tkinter.Button(root, text="Quit", width=8,
     command=root.quit)
-btn.pack()
+btn.pack(pady=10)
 
 root.geometry("300x250+300+300")
 root.mainloop()
