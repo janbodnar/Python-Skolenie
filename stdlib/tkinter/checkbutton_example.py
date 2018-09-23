f36837a997192dabbfa198a9fc0d766855409386
Jan Bodnar <jan.bodnar@gmail.com>
2018-09-23 22:25:56 +0200

    Update checkbutton_example.py

--- stdlib/tkinter/checkbutton_example.py
+++ stdlib/tkinter/checkbutton_example.py
@@ -19,7 +19,7 @@ cbvar = tkinter.BooleanVar()
 cbtn = tkinter.Checkbutton(root, text="Show", width=8, variable=cbvar,
     command=toggleTitle)
 cbtn.select()
-cbtn.pack()
+cbtn.pack(pady=10)
 
 root.geometry("300x250+300+300")
 root.mainloop()
