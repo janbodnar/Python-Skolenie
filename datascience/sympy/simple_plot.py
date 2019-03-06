7534f0dea71afdf0e4ef6d0e27cc41f014d594be
Jan Bodnar <jan.bodnar@gmail.com>
2019-03-06 12:46:17 +0100

    Update simple_plot.py

--- datascience/sympy/simple_plot.py
+++ datascience/sympy/simple_plot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-# uses matplotlib
+# uses matplotlib 
 
 from sympy import symbols, cos
 from sympy.plotting import plot3d
 
 x, y = symbols('x y')
+
 plot3d(cos(x*3)*cos(y*5)-y)
