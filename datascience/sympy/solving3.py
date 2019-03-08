817a8044c3cb575c7d30ba83241acefe5253dc56
Jan Bodnar <jan.bodnar@gmail.com>
2019-03-08 19:36:12 +0100

    Create solving3.py

---
+++ datascience/sympy/solving3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from sympy.solvers import solveset
+from sympy import Symbol, Interval, pprint
+
+x = Symbol('x')
+
+sol = solveset(x**2 - 1, x, Interval(0, 100))
+print(sol)
