a49685907510d6c8e0cac538e60a2d2dbf3041cc
Jan Bodnar <jan.bodnar@gmail.com>
2019-03-08 19:34:25 +0100

    Create sequence.py

---
+++ datascience/sympy/sequence.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from sympy import summation, sequence, pprint
+from sympy.abc import x
+
+s = sequence(x, (x, 1, 10))
+print(s)
+pprint(s)
+print(list(s))
+
+print(s.length)
+
+print(summation(s.formula, (x, s.start, s.stop)))
+# print(sum(list(s)))
