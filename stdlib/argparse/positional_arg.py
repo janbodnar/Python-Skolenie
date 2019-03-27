463403c15baac95fd4697c93942cf2f79ce47a00
Jan Bodnar <jan.bodnar@gmail.com>
2019-03-27 13:28:31 +0100

    Create positional_arg.py

---
+++ stdlib/argparse/positional_arg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import argparse
+
+# positional args
+
+parser = argparse.ArgumentParser()
+   
+parser.add_argument('name')
+parser.add_argument('age')
+
+args = parser.parse_args()
+
+print(f'{args.name} is {args.age} years old')
