0643c4955f26d663f4dd8a985baf7b4b1174e623
Jan Bodnar <jan.bodnar@gmail.com>
2019-04-10 14:30:42 +0200

    Create mailtrap_attachment.py

---
+++ stdlib/smtplib/mailtrap_attachment.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import smtplib
+from os.path import basename
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+sender = 'admin@example.com'
+receiver = 'info@example.com'
+
+msg = MIMEMultipart()
+
+msg['Subject'] = 'Test mail with attachment'
+msg['From'] = 'admin@example.com'
+msg['To'] = 'info@example.com'
+
+filename = 'words.txt'
+with open(filename, 'r') as f:
+    part = MIMEApplication(f.read(), Name=basename(filename))
+
+part['Content-Disposition'] = 'attachment; filename="{}"'.format(basename(filename))
+msg.attach(part)
+
+user = 'username'
+password = 'password'
+
+with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
+
+    server.login(username, password)
+    server.sendmail(sender, receiver, msg.as_string())
+    print("Successfully sent email")
