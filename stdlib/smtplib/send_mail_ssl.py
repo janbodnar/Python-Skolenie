c085c68db4886baf154660fa6dd4995abc0e8bf3
Jan Bodnar <jan.bodnar@gmail.com>
2019-04-10 14:27:05 +0200

    Create send_mail_ssl.py

---
+++ stdlib/smtplib/send_mail_ssl.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import smtplib, ssl
+from email.mime.text import MIMEText
+
+sender = 'admin@zetcode.com'
+receivers = ['admin2@zetcode.com']
+
+port = 465
+user = 'admin@zetcode.com'
+password = '17ttZdLjpY'
+
+msg = MIMEText('This is test mail')
+
+msg['Subject'] = 'Test mail'
+msg['From'] = 'admin@example.com'
+msg['To'] = 'info@example.com'
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.websupport.sk", port, context=context) as server:
+
+    server.login(user, password)
+    server.sendmail(sender, receivers, msg.as_string())
+    print('mail successfully sent')
+
