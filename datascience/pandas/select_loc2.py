8aa4d44dea04cab1fd2c916ade0ba67408d1a266
Jan Bodnar <jan.bodnar@gmail.com>
2019-05-16 20:46:02 +0200

    Create select_loc2.py

---
+++ datascience/pandas/select_loc2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+data = {'Items': ['coins', 'pens', 'books'], 'Quantity': [22, 28, 3]}
+
+df = pd.DataFrame(data, index=['A', 'B', 'C'])
+
+print(df.loc[[True, False, True], ['Items', 'Quantity']])
