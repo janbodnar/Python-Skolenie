c2b246e3d478d8df31b9db6111fb902d37a0c358
Jan Bodnar <jan.bodnar@gmail.com>
2019-05-16 16:13:06 +0200

    Create head_tail.py

---
+++ datascience/pandas/head_tail.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import pandas as pd 
+  
+df = pd.read_csv("military_spending.csv") 
+
+print(df.head(4))
+
+print('*******************************************')
+
+print(df.tail(4))
