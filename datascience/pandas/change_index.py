94280cdc21322ade0fc237f570a58470966ec535
Jan Bodnar <jan.bodnar@gmail.com>
2019-05-16 12:29:00 +0200

    Create change_index.py

---
+++ datascience/pandas/change_index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+data = [['Alex', 10], ['Ronald', 18], ['Jane', 33]]
+df = pd.DataFrame(data, columns=['Name', 'Age'])
+df.index = df.index + 1
+
+print(df)
