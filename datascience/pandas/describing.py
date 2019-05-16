2ae2602937713c000dfa10f8ef7a51514a7bae55
Jan Bodnar <jan.bodnar@gmail.com>
2019-05-16 15:09:41 +0200

    Create describing.py

---
+++ datascience/pandas/describing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+s1 = pd.Series([1, 2, 3, 4, 5, 6, 7, 8])
+s2 = pd.Series([12, 23, 31, 14, 11, 61, 17, 18])
+
+data = {'Vals 1': s1, 'Vals 2': s2}
+df = pd.DataFrame(data)
+
+print(df.describe())
