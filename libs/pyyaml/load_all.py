ec510030cb31228fb96c3808775517df37238d00
Jan Bodnar <jan.bodnar@gmail.com>
2019-05-24 13:31:04 +0200

    Create load_all.py

---
+++ libs/pyyaml/load_all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import yaml
+
+with open('data.yaml') as f:
+    
+    docs = yaml.load_all(f, Loader=yaml.FullLoader)
+
+    for doc in docs:
+        
+        for k, v in doc.items():
+            print(k, "->", v)
