ace3799377452dbd9e1e1439bdf020068e1cf314
Jan Bodnar <jan.bodnar@gmail.com>
2019-05-24 13:31:17 +0200

    Create dumping.py

---
+++ libs/pyyaml/dumping.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import yaml
+
+users = [{'name': 'John Doe', 'occupation': 'gardener'},
+         {'name': 'Lucy Black', 'occupation': 'teacher'}]
+
+print(yaml.dump(users))
