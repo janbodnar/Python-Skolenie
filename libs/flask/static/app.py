9fd8f8d1a5a05c4bf311b246136d9002df55cf53
Jan Bodnar <jan.bodnar@gmail.com>
2019-08-18 16:14:02 +0200

    Create app.py

---
+++ libs/flask/static/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
