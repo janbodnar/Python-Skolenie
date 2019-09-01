f4ed0ad7a012468c7c1ce5244c71b15eed7e9402
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-01 09:38:35 +0200

    Create min_max.py

---
+++ libs/cerberus/min_max.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from cerberus import Validator
+
+v = Validator()
+v.schema = {'name': { 'type': 'string', 'minlength': 2}, 
+    'age': {'type': 'integer', 'min': 18, 'max': 65}}
+
+if v.validate({'name': 'J', 'age': 4}):
+    print('valid data')
+else:
+    print('invalid data')
+    print(v.errors)
