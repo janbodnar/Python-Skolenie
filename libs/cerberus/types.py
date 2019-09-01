1404c573689d5fda1f069cc10c1aa77b23467f38
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-01 09:40:11 +0200

    Create types.py

---
+++ libs/cerberus/types.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from cerberus import Validator
+
+
+v = Validator()
+v.schema = {'words': {'type': ['string', 'list']}}
+
+if v.validate({'words': 'falcon'}):
+    print('valid data')
+else:
+    print('invalid data')
+
+if v.validate({'words': ['falcon', 'sky', 'cloud']}):
+    print('valid data')
+else: 
+    print('invalid data')
