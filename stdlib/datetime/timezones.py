26c8f154e5980ecc94143666a5221a5c6b862713
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-15 22:00:58 +0200

    Create timezones.py

---
+++ stdlib/datetime/timezones.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+
+import pytz
+local = datetime.now()
+
+print("Local:", local.strftime("%m/%d/%Y, %H:%M:%S"))
+tz_MSC = pytz.timezone('Europe/Moscow') 
+
+datetime_NY = datetime.now(tz_MSC)
+print("Moscow:", datetime_NY.strftime("%m/%d/%Y, %H:%M:%S"))
+
+tz_London = pytz.timezone('Europe/London')
+datetime_London = datetime.now(tz_London)
+
+print("London:", datetime_London.strftime("%m/%d/%Y, %H:%M:%S"))
