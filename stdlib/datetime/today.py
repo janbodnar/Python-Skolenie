cb5c6827799c5d2f3c8ebf05e77b1c25754f2390
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-15 21:40:10 +0200

    Create today.py

---
+++ stdlib/datetime/today.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from datetime import date
+
+today = date.today() 
+
+print("Current year: ", today.year)
+print("Current month: ", today.month)
+print("Current day: ", today.day)
