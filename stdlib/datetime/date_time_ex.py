773029d5dce213befbd2b8ab0224caabaa91e10a
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-15 22:08:28 +0200

    Create date_time_ex.py

---
+++ stdlib/datetime/date_time_ex.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+a = datetime(2019, 9, 15, 22, 3, 31, 355741)
+
+print(f'year: {a.year}')
+print(f'month: {a.month}')
+print(f'hour: {a.hour}')
+print(f'minute: {a.minute}')
+print(f'timestamp: {a.timestamp()}')
