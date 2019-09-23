996be68c2f8c146a1c9285398b875039110fee61
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-23 14:13:11 +0200

    Update object_initialization.py

--- oop/object_initialization.py
+++ oop/object_initialization.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 # object_initialization.py
 
 class Being:
     
     def __init__(self):
-        print ("Being is initialized")
+        print("Being is initialized")
 
 Being()
