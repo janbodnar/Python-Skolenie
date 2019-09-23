29386c9693e3ae37e38bbd65e7cbc2c9fa4730c9
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-23 14:13:41 +0200

    Update class_attribute.py

--- oop/class_attribute.py
+++ oop/class_attribute.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 # class_attribute.py
 
@@ -14,9 +14,9 @@ class Cat:
 missy = Cat('Missy', 3)
 lucky = Cat('Lucky', 5)
 
-print (missy.name, missy.age)
-print (lucky.name, lucky.age)
+print(missy.name, missy.age)
+print(lucky.name, lucky.age)
 
-print (Cat.species)
-print (missy.__class__.species)
-print (lucky.__class__.species)
+print(Cat.species)
+print(missy.__class__.species)
+print(lucky.__class__.species)
