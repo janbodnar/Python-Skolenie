812f52c8f69291042b2e21d7f7ae92e0dd1c0875
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-29 19:49:26 +0200

    Create walking.py

---
+++ new/walking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+# list all Python files 
+
+
+if len(sys.argv) != 2:
+    
+    print('Usage: list_files.py dir_name')
+    sys.exit(1)
+
+dir_name = sys.argv[1]
+
+gfiles = os.walk(dir_name)
+
+for (root, dir, files) in gfiles:
+
+    for file in files:
+
+        if file.endswith('py'):
+            print(file)
