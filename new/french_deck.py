41e2fac03015eed10dae9fb3801881b9d32eb2f0
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-29 19:41:24 +0200

    Create french_deck.py

---
+++ new/french_deck.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import collections
+from random import choice
+
+
+Card = collections.namedtuple('Card', ['suit', 'rank'])
+
+
+class FrenchDeck:
+
+    ranks = [str(i) for i in range(2, 11)] + list('JQKA')
+    suits = ["heart", "clubs", "spades", "diamond"]
+
+    def __init__(self):
+        self.total = [Card(suit, rank)
+                           for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self.total)
+
+    def __getitem__(self, index):
+        return self.total[index]
+
+
+deck = FrenchDeck()
+print(deck[0])
+# print(deck[2:7])
+# print(deck[-2])
+print(len(deck))
+print(choice(deck))
+
+# namedtuple is a factory function for making a tuple class. With that class we
+# can create tuples that are callable by name also.
+
+# It's a specific subclass of a tuple that is programmatically created to your
+# specification, with named fields and a fixed length.
