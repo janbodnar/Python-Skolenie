1bc1344ad58fe0ba1c58fd8b4f5d8bad26353f4b
Jan Bodnar <jan.bodnar@gmail.com>
2019-09-29 19:44:05 +0200

    Create operator_fun.py

---
+++ new/operator_fun.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from operator import add, sub, mul, mod
+
+x = 2
+y = 4
+
+w = add(x, y)
+print(w)
+
+w = sub(x, y)
+print(w)
+
+w = mul(x, y)
+print(w)
+
+w = sub(y, x)
+print(w)
