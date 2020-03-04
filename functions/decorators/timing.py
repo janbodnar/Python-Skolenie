22fba54a75f869a7d3746762244eb886b3e4409d
Jan Bodnar <jan.bodnar@gmail.com>
2020-03-04 17:14:07 +0100

    Create timing.py

---
+++ functions/decorators/timing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import time
+import math
+
+def timer(func):
+
+    def wrapper(*args, **kwargs):
+
+        begin = time.time()
+
+        f = func(*args, **kwargs)
+
+        end = time.time()
+        print("Total time taken in : ", func.__name__, end - begin)
+
+        return f
+
+    return wrapper
+
+
+@timer
+def factorial(num):
+
+    return math.factorial(num)
+
+f = factorial(9580)
+print(f)
