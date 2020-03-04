b0d6357cf3ab3d21de870bfaeb9601d469be2c9f
Jan Bodnar <jan.bodnar@gmail.com>
2020-03-04 14:29:32 +0100

    Create returning_fun.py

---
+++ functions/returning_fun.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Python allows to return nested functions
+
+def do_output(shape):
+
+    def rectangle():
+
+        return '''
+*********
+*********'''
+
+    def triangle():
+
+        return '''
+  *
+ ***
+*****'''
+
+    if shape == 'rectangle':
+        return rectangle
+
+    if shape == 'triangle':
+        return triangle
+
+
+f = do_output('triangle')
+print(f())
+
+print()
+
+f = do_output('rectangle')
+print(f())
