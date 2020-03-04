2b971f6cd163ff4d5ae6a16d47265dbba648e0f5
Jan Bodnar <jan.bodnar@gmail.com>
2020-03-04 17:12:08 +0100

    Create enclose.py

---
+++ functions/decorators/enclose.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def enclose(fun):
+
+    def wrapper():
+
+        print("***************************")
+        fun()
+        print("***************************")
+
+    return wrapper
+
+def myfun():
+    print("myfun")
+
+enc = enclose(myfun)
+enc()
