7b0ce84b0a290dda0f5967cd7b505d283de531e5
Jan Bodnar <jan.bodnar@gmail.com>
2020-03-04 17:12:40 +0100

    Create multiple_decor.py

---
+++ functions/decorators/multiple_decor.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+def start(fun):
+
+    def inner():
+
+        print("***************************")
+        fun()
+
+    return inner
+
+
+def end(fun):
+
+    def inner():
+
+        fun()
+        print("***************************")
+
+    return inner
+
+@start
+@end
+def myfun():
+    print("myfun")
+
+myfun()
+
+# def myfun():
+#     print("myfun")
+
+# f = start(end(myfun))
+# f()
