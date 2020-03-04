b631d258a443d89daa8e15934eebeeffa948991a
Jan Bodnar <jan.bodnar@gmail.com>
2020-03-04 17:11:26 +0100

    Create static_method.py

---
+++ functions/decorators/static_method.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class Math:
+
+    @staticmethod
+    def abs(x):
+
+        if x < 0:
+            return -x
+        return x
+
+
+print(Math.abs(3))
+print(Math.abs(-3))
