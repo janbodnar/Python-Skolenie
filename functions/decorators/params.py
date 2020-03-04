405f039ae6e766bda751fd475667bd892688816b
Jan Bodnar <jan.bodnar@gmail.com>
2020-03-04 17:13:20 +0100

    Create params.py

---
+++ functions/decorators/params.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def enclose(fun):
+
+    def wrapper(*args, **kwargs):
+
+        print("***************************")
+        fun(*args, **kwargs)
+        print("***************************")
+
+    return wrapper
+
+@enclose
+def myfun(name, age):
+    print(f'{name} is {age} years old')
+
+myfun(name='Peter', age=32)
+myfun('Roman', 29)
