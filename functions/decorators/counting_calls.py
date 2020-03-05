0035bd1424fa3b4821fd8e1027698aa27b7b16ac
Jan Bodnar <jan.bodnar@gmail.com>
2020-03-05 10:17:11 +0100

    Create counting_calls.py

---
+++ functions/decorators/counting_calls.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import functools
+
+class CountCalls:
+
+    def __init__(self, fun):
+
+        functools.update_wrapper(self, fun)
+        self.fun = fun
+        self.num_of_calls = 0
+
+    def __call__(self, *args, **kwargs):
+
+        self.num_of_calls += 1
+        print(f"Call {self.num_of_calls} of {self.fun.__name__} fun")
+        return self.fun(*args, **kwargs)
+
+@CountCalls
+def hello():
+    print("Hello there!")
+
+hello()
+hello()
+hello()
