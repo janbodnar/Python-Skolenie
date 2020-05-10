92c396730aaa8f8482b81488fa0f75cbecc6feb8
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-10 15:11:28 +0200

    Create calc_pi_seq.py

---
+++ stdlib/multiprocessing/calc_pi_seq.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from decimal import Decimal, getcontext
+from timeit import default_timer as timer
+
+# calculating pi sequentially with 
+# Bailey–Borwein–Plouffe formula
+
+
+def pi(precision):
+
+    getcontext().prec = precision
+
+    return sum(1/Decimal(16)**k *
+        (Decimal(4)/(8*k+1) -
+         Decimal(2)/(8*k+4) -
+         Decimal(1)/(8*k+5) -
+         Decimal(1)/(8*k+6)) for k in range (precision))
+
+
+start = timer()
+values = (1000, 1500, 2000)
+data = list(map(pi, values))
+print(data)
+
+end = timer()
+print(f'sequentially: {end - start}')
