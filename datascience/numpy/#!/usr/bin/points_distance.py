71d59c4b666589cd7ea2aa7eb68087e1151875ed
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-12 13:48:18 +0200

    Create points_distance.py

---
+++ datascience/numpy/#!/usr/bin/points_distance.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+# three points
+points = np.array([[-1, 1], [1, 0], [2, 0]])
+
+# distance between all points
+d = squareform(pdist(points, 'euclidean'))
+print(d)
