0b28fb38af45a807955c5b5bbf1e6966b87dcf7c
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-12 15:57:58 +0200

    Create from_function.py

---
+++ datascience/numpy/from_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+# Construct an array by executing a function over each coordinate.
+
+def f(x, y):
+    return 2*x + y + 1
+
+a = np.fromfunction(f, (5, 4), dtype=int)
+print(a)
+
+# anonymous functoin
+b = np.fromfunction(lambda x, y: 2*x + y, (2, 2))
+print(b)
