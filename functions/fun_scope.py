80fa2131a311f394c6c2ca429c71ac4d01567387
Jan Bodnar <jan.bodnar@gmail.com>
2023-08-26 12:16:44 +0200

    Update and rename local_variable.py to fun_scope.py

--- functions/fun_scope.py
+++ functions/fun_scope.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# local_variable.py
+# fun_scope.py
 
 # a local variable is valid in the 
 # function scope
@@ -9,7 +9,7 @@ name = "Jack"
 
 def f():
    name = "Robert"
-   print ("Within function", name)
+   print("Within function", name)
 
-print ("Outside function", name)
+print("Outside function", name)
 f()
