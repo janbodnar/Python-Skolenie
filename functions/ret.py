6c0c2701deef07d05b38f3c297e74d3fca71310b
Jan Bodnar <jan.bodnar@gmail.com>
2023-08-26 12:16:08 +0200

    Update ret.py

--- functions/ret.py
+++ functions/ret.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python
 
-"""
-The ret.py script shows how to work with
-functions in Python. 
-Author: Jan Bodnar
-ZetCode, 2016
-"""
-
 def showModuleName():
     
     print (__doc__)
@@ -18,4 +11,4 @@ def getModuleFile():
 a = showModuleName()
 b = getModuleFile()
 
-print (a, b)
+print(a, b)
