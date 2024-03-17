5e057332d8682525a5cba95835891940cb8853b5
Jan Bodnar <jan.bodnar@gmail.com>
2024-03-17 10:55:22 +0100

    Delete stdlib/datetime/parsing.py

--- stdlib/datetime/parsing.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from datetime import datetime
-
-dt1 = datetime.strptime('Jun 1 2018 5:33PM', '%b %d %Y %I:%M%p')
-print(dt1)
-
-dt2 = datetime.strptime('23:33:45', '%H:%M:%S')
-print(dt2.time())
