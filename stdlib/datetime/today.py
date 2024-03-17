e7e6ea804e922473abd70786c4d32e5c85e3413b
Jan Bodnar <jan.bodnar@gmail.com>
2024-03-17 10:53:39 +0100

    Delete stdlib/datetime/today.py

--- stdlib/datetime/today.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from datetime import date
-
-today = date.today() 
-
-print("Current year: ", today.year)
-print("Current month: ", today.month)
-print("Current day: ", today.day)
