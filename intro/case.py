b05d263707970b1eec2144cd2574453303b8b577
Jan Bodnar <jan.bodnar@gmail.com>
2024-11-03 20:09:00 +0100

    deletes

--- intro/case.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-number = 10
-Number = 11
-NUMBER = 12
-
-print (number, Number, NUMBER)
--- intro/read_input.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-# read_input.py
-
-name = input("Enter your name:")
-print ("Hello", name)
