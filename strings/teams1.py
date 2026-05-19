3f9d453ac9bcc640a178afb24d93f8fe1b27d5c9
Jan Bodnar <jan.bodnar@gmail.com>
2026-05-19 10:42:59 +0200

    Delete strings/teams1.py

--- strings/teams1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-# teams1.py
-
-print ("Ajax Amsterdam" " - " + "Inter Milano " "2:3")
-print ("Real Madridi" " - " "AC Milano " "3:3")
-print ("Dortmund" " - " "Sparta Praha " "2:1")
