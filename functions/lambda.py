bb864e0304f4ce6e9a0cb7de748827c75f39bda2
Jan Bodnar <jan.bodnar@gmail.com>
2026-09-01 13:33:18 +0200

    deleted function directory

--- functions/lambda.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-# lambda.py
-
-y = 6
-
-z = lambda x: x * y
-print (z(8))
--- functions/lambda2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-# lambda2.py
-
-cs = [-10, 0, 15, 30, 40]
-
-ft = map(lambda t: (9.0/5)*t + 32, cs)
-print (list(ft))
